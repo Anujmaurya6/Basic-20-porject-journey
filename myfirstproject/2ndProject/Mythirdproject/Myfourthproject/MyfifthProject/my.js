b9d637e986b1190aac872572b06069e7a6dc0648
anujmaurya6 <maurya913639@gmail.com>
2025-07-27 22:38:17 +0530

    PROJECT MINI   SERIES

--- myfirstproject/2ndProject/Mythirdproject/Myfourthproject/MyfifthProject/my.js
+++ myfirstproject/2ndProject/Mythirdproject/Myfourthproject/MyfifthProject/my.js
@@ -1,35 +1,46 @@
-function loadNotes() {
-  const notes = JSON.parse(localStorage.getItem("notes")) || [];
-  const container = document.getElementById("notes-container");
-  container.innerHTML = "";
-  notes.forEach((note, index) => {
-    const div = document.createElement("div");
-    div.className = "note";
-    div.innerHTML = `
-      ${note}
-      <button onclick="deleteNote(${index})">×</button>
+//pehle se saved notes ko load karo jab page load ho
+window.load=function(){
+  showNotesOnScreen();
+}
+//add note jab user kuch likhe aur button dabaye 
+function addNote(){
+  const inputbox=document.getElementById("user-input");
+  const noteText=inputbox.value.trim();
+  if(noteText === " "){
+    alert("the note cannot be empty!");
+    return;
+  }
+
+  //pehle se saved notes uthao ya khali array banao
+  let notes=JSON.parse(localStorage.getItem("notes"))||[];
+
+  //naya note add karo
+  notes.push(noteText)
+
+  //localstorage me update karunga
+  localStorage.setItem("notes",JSON.stringify(notes));
+
+  //input box clear karo and screen update karo 
+  inputbox.value="";
+  showNotesOnScreen();
+}
+//screen pe sab notes dikahne ka kaam
+function showNotesOnScreen(){
+  const container=document.getElementById("notes-container");
+  container.innerHTML=""; //purane display ko clear karo
+
+  const notes=JSON.parse(localStorage.getItem("notes")) || [];
+
+  notes.forEach((note,Index) => {
+        const noteDiv = document.createElement("div");
+    noteDiv.className = "note";
+
+    noteDiv.innerHTML = `
+      <p>${note}</p>
+      <button onclick="deleteNote(${index})">❌</button>
     `;
-    container.appendChild(div);
+
+    container.appendChild(noteDiv);
+    
   });
 }
-
-function addNote() {
-  const input = document.getElementById("note-input");
-  const note = input.value.trim();
-  if (note !== "") {
-    const notes = JSON.parse(localStorage.getItem("notes")) || [];
-    notes.push(note);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    input.value = "";
-    loadNotes();
-  }
-}
-
-function deleteNote(index) {
-  const notes = JSON.parse(localStorage.getItem("notes")) || [];
-  notes.splice(index, 1);
-  localStorage.setItem("notes", JSON.stringify(notes));
-  loadNotes();
-}
-
-window.onload = loadNotes;
